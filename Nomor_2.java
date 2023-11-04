7cc7cf33ed1791fad4784468a3f815613484de21
Valgiyos <147407550+Valgiyos@users.noreply.github.com>
2023-11-04 22:26:23 +0700

    Update Nomor_2.java

--- Nomor_2.java
+++ Nomor_2.java
@@ -1,53 +1,23 @@
 import java.util.Scanner;
 
-public class Nomor_2 {
-
+public class WhileLoopExample {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        char opsi;
-        double bilangan1, bilangan2;
-        double hasil = 0;
+        Scanner scanner = new Scanner(System.in);
+        int number = 0;
 
-        while (true) {
-            System.out.println("\nPilih operasi matematika:");
-            System.out.println("0. Keluar");
-            System.out.println("1. Penjumlahan (+)");
-            System.out.println("2. Pengurangan (-)");
-            System.out.println("3. Perkalian (x)");
-            System.out.println("4. Pembagian (:)");
-            System.out.print("\nMasukkan pilihan: ");
-            opsi = input.next().charAt(0);
+        while (number < 5) {
+            System.out.print("Masukkan sebuah angka (0-4): ");
+            number = scanner.nextInt();
 
-            if (opsi == '0') {
-                System.out.println("\nTerima kasih :)");
-                break;
-            }
-
-            if (opsi >= '1' && opsi <= '4') {
-                System.out.print("Bilangan ke-1: ");
-                bilangan1 = input.nextDouble();
-                System.out.print("Bilangan ke-2: ");
-                bilangan2 = input.nextDouble();
-
-                if (opsi == '1') {
-                    hasil = bilangan1 + bilangan2;
-                } else if (opsi == '2') {
-                    hasil = bilangan1 - bilangan2;
-                } else if (opsi == '3') {
-                    hasil = bilangan1 * bilangan2;
-                } else if (opsi == '4') {
-                    if (bilangan2 != 0) {
-                        hasil = bilangan1 / bilangan2;
-                    } else {
-                        System.out.println("Tak terdefinisi");
-                        continue;
-                    }
-                }
-                System.out.println("HASIL: " + hasil);
+            if (number < 0 || number > 4) {
+                System.out.println("Angka tidak valid, silakan coba lagi.");
+            } else if (number % 2 == 0) {
+                System.out.println("Angka genap.");
             } else {
-                System.out.println("Pilihan hanya dari nomor 1-4");
+                System.out.println("Angka ganjil.");
             }
         }
-        input.close();
+
+        scanner.close();
     }
 }
