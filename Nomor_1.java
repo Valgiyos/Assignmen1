9cd0c00e62dcbe32ff3f9db0ca9c368d622ec1d8
Valgiyos <147407550+Valgiyos@users.noreply.github.com>
2023-11-04 22:20:04 +0700

    Create Nomor_1.java

---
+++ Nomor_1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Nomor_1{
+
+    public static void main(String[] args) {
+
+        for (int nilai = 1; nilai <= 9; nilai++) {
+
+            System.out.println(nilai);
+        }
+        for (int nilai = 10; nilai <= 100; nilai++){
+
+            System.out.println("M.D Valgiyos Aritonang");
+        }
+    }
+}
