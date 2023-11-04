cfecbb751b4766cbb368996f47838747123dff66
Valgiyos <147407550+Valgiyos@users.noreply.github.com>
2023-11-04 22:25:10 +0700

    Update Nomor_1.java

--- Nomor_1.java
+++ Nomor_1.java
@@ -1,16 +1,11 @@
-import java.util.Scanner;
-
-public class Nomor_1{
-
+public class NumberList {
     public static void main(String[] args) {
-
-        for (int nilai = 1; nilai <= 9; nilai++) {
-
-            System.out.println(nilai);
-        }
-        for (int nilai = 10; nilai <= 100; nilai++){
-
-            System.out.println("M.D Valgiyos Aritonang");
+        for (int i = 1; i <= 100; i++) {
+            if (i <= 9) {
+                System.out.println(i);
+            } else {
+                System.out.println("M.D Valgiyos Aritonang");
+            }
         }
     }
 }
